e1e3c4e2bc8bdfb9b0c95453fbad3454f9342b9f
latest123 <latest123@linuxacademy.com>
2018-02-16 01:48:54 +0000

    updated default.rb

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+if node['platform_family'] == "rhel"
+        package = "httpd"
+elsif node['platform_family'] == "debian"
+        package = "apache2"
+end
+
+package 'apache' do
+   package_name httpd
+   action :install
+end
+
+service 'apache' do
+   service_name 'httpd'
+   action[:start, :enable]
+end
+
+
